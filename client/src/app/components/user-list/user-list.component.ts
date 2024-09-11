50d5407e8159b6d64e5561639eea88f44741a182
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-09-10 22:14:46 -0500

    fetched server data and displayed it on client

--- client/src/app/components/user-list/user-list.component.ts
+++ client/src/app/components/user-list/user-list.component.ts
@@ -1,13 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  httpClient = inject(HttpClient);
+  data: any[] = [];
+  apiUrl = 'http://localhost:3000/';
 
+  ngOnInit(): void { 
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.httpClient
+      .get(this.apiUrl)
+      .subscribe((data: any) => {
+        this.data = data;
+      });
+  }
 }
